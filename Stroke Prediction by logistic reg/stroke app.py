e6371fae9747e38b4f996c142f02b12afadcbae4
S.Sundaresh <102817921+sundaresh98@users.noreply.github.com>
2022-07-08 16:14:33 +0530

    Add files via upload

---
+++ Stroke Prediction by logistic reg/stroke app.py
@@ -0,0 +1,51 @@
+import  numpy as np
+from flask import Flask, render_template, request
+import joblib
+import pickle
+import os
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+@app.route("/result",methods=['POST','GET'])
+def result():
+    gender = int(request.form['gender'])
+    age = int(request.form['age'])
+    hypertension = int(request.form['hypertension'])
+    heart_disease = int(request.form['heart_disease'])
+    ever_married = int(request.form['ever_married'])
+    work_type = int(request.form['work_type'])
+    Residence_type = int(request.form['Residence_type'])
+    avg_glucose_level = float(request.form['avg_glucose_level'])
+    bmi = float(request.form['bmi'])
+    smoking_status = int(request.form['smoking_status'])
+
+    x = np.array([gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type,avg_glucose_level,bmi,smoking_status]).reshape(1,-1)
+
+    scaler_path = os.path.join('Healthcare.csv', 'models/stroke.pkl')
+    scaler = None
+    with open(scaler_path, 'rb') as scaler_file:
+        scaler = pickle.load(scaler_file)
+    x = scaler.transform(x)
+    model_path = os.path.join('Healthcare.csv', 'models/dt.sav')
+    dt = joblib.load(model_path)
+
+    Y_pred = dt.predict(x)
+
+    if Y_pred == 0:
+        return render_template('nostroke.html')
+    else:
+        return render_template('stroke.html')
+
+if __name__=="__main__":
+    app.run(debug=True, port=7224)
+
+
+
+
+
+
+
+
